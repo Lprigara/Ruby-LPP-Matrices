a58604cebfbe39517abbcdd41cf83bb9b0acb370
Leonor Priego <lprigara@gmail.com>
2014-03-22 16:15:03 +0000

    Diseño de DSL interno para trabajar con matrices. Uso de Gemnasium

---
+++ Practica12/lib/prac09.rb
@@ -0,0 +1,10 @@
+require "prac09/version"
+
+
+module Prac09
+require "prac09/fraccion"
+require "prac09/matriz"
+require "prac09/matrizdispersa"
+require "prac09/matrizdensa"
+require "prac09/dsl"
+end
